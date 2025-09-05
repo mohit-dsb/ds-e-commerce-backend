6bd0ec9bb00c7cbb1e1a3e6ae97fce364acec9ca
Mohit Dsb <mohit.mourya@designingsolutions.co.in>
2025-09-05 12:05:42 +0530

    temp cookie fix

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use("*", honoLogger());
 app.use(
   "*",
   cors({
-    origin: env.NODE_ENV === "production" ? env.CORS_ORIGIN : "*",
+    origin: "http://localhost:5173",
     allowMethods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
     credentials: true,
